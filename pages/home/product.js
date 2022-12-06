a833c33cfd69bd109a3b6dc1ae188b9b07122b8b
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2022-12-06 12:12:04 +0500

    recent api click not working properly

--- pages/home/product.js
+++ pages/home/product.js
@@ -1,5 +1,3 @@
-import CustomButton from "../../components/Misc/CustomButton";
-import Heading from "../../components/Misc/Heading";
 import { getAllAPIsApi } from "../../utils/ApiCalls";
 import { checkStatus, getGeneralApiParams } from "../../utils/GeneralVariables";
 import { addToRecent, selectTabs } from "../../Redux/Recents/recentsSlice";
@@ -7,10 +5,9 @@ import { addToRecent, selectTabs } from "../../Redux/Recents/recentsSlice";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { BsThreeDotsVertical } from "react-icons/bs";
 import { FiChevronRight } from "react-icons/fi";
 import { IoNavigate } from "react-icons/io5";
-import removeWhitespace from "remove-whitespace";
+import { MdDelete } from "react-icons/md";
 
 const ProductAPIPage = () => {
 	const router = useRouter();
@@ -25,7 +22,7 @@ const ProductAPIPage = () => {
 			let status = checkStatus(response, "");
 			// to get the unique category names from all the apis
 			const unique = [
-				...new Set(response.data.data.apis.map((each) => each.category)),
+				...new Set(response?.data?.data?.apis?.map((each) => each.category)),
 			];
 			status ? setSelected(unique[0]) : "";
 			status ? setCategories(unique) : "";
@@ -51,26 +48,28 @@ const ProductAPIPage = () => {
 					<h3 className="p-6 font-nunito font-bold text-lg border-b-[1px] text-txt-dark">
 						Recent APIs
 					</h3>
-					<div>
-						<div className="overflow-y-hidden h-96">
-							{recents?.map((each) => (
-								<section
-									key={each.endpoint}
-									className="grid justify-center items-center grid-cols-8 hover:bg-white"
-								>
-									<div className="col-span-7 overflow-y-auto py-4 px-2 duration-300 ">
-										<p className="select-none px-4">{each.name}</p>
+					<div className="overflow-y-hidden h-96">
+						{recents?.map((each) => (
+							<section
+								key={each.endpoint}
+								className="flex flex-row w-full justify-between items-center pr-4 hover:bg-gray-300 cursor-pointer duration-200 animate-dropdown"
+								onClick={() => {
+									handleRoute(each)
+								}}
+							>
+								<div className="col-span-7 overflow-y-auto py-4 px-2 duration-300 ">
+									<p className="select-none px-4">{each.name}</p>
+								</div>
+								<div className="flex space-x-4">
+									<div className="cursor-pointer relative">
+										<IoNavigate size={24} />
 									</div>
 									<div className="cursor-pointer relative">
-										<BsThreeDotsVertical
-											size={24}
-											className="hover:fill-main-blue hover:scale-[120%] duration-300"
-										/>
-										{/* <div>Go to</div> */}
+										<MdDelete size={24} color="red" />
 									</div>
-								</section>
-							))}
-						</div>
+								</div>
+							</section>
+						))}
 					</div>
 				</div>
 				<aside className="grid grid-cols-3 bg-white shadow-xl border-slate-100 border-[1px] mx-2 w-[70%]">
@@ -96,17 +95,8 @@ const ProductAPIPage = () => {
 											</div>
 											<div className="cursor-pointer  relative">
 												<FiChevronRight size={24} className="" />
-												{/* <div>Go to</div> */}
 											</div>
 										</div>
-										{/* <div className="">
-										<CustomButton
-											width={"1/3"}
-											onClick={() => handleRoute(each)}
-										>
-											Click
-										</CustomButton>
-									</div> */}
 									</section>
 								))}
 							</div>
@@ -132,17 +122,8 @@ const ProductAPIPage = () => {
 												</div>
 												<div className="cursor-pointer  relative">
 													<IoNavigate size={24} className="animate-dropdown" />
-													{/* <div>Go to</div> */}
 												</div>
 											</div>
-											{/* <div className="">
-										<CustomButton
-											width={"1/3"}
-											onClick={() => handleRoute(each)}
-										>
-											Click
-										</CustomButton>
-									</div> */}
 										</section>
 									))}
 							</div>
