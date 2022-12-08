8e15333b505ca60727e67f76216d8818e42455f7
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2022-12-08 14:03:04 +0500

    clicking label focuses on input

--- components/Misc/CustomInput.js
+++ components/Misc/CustomInput.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useRef } from "react";
 
 function CustomInput({
 	type,
@@ -15,6 +16,8 @@ function CustomInput({
 	disabled,
 	heading,
 }) {
+	const inputRef = useRef()
+
 	return (
 		<div className="relative m-2">
 			<input
@@ -30,8 +33,13 @@ function CustomInput({
 				id="floating_outlined"
 				className="z-20 block px-2.5 pb-2.5 pt-2 w-full text-sm text-gray-900 bg-transparent rounded-md border-[1.5px] appearance-none border-slate-300 focus:outline-none focus:ring-0 focus:border-main-blue peer"
 				placeholder=" "
+				ref={inputRef}
 			/>
-			<label className="select-none absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-main-blue peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">
+			<label className="select-none absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-main-blue peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+				onClick={() => {
+					inputRef.current.focus()
+				}}
+			>
 				{heading}
 			</label>
 		</div>
