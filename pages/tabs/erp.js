71589705283707b29748f0a654b443909f68a1c6
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2023-01-05 15:07:19 +0500

    made erp page

---
+++ pages/tabs/erp.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MainTabComponent from "./main";
+
+const ERPManagementPage = () => {
+	return (
+		<section className="">
+			<div>
+				<MainTabComponent />
+			</div>
+		</section>
+	);
+};
+
+export default ERPManagementPage;
