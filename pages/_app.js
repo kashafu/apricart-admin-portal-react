e506bf29a27548e6e8e15ddda1269c09bcd9ffb2
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2023-01-13 11:54:28 +0500

    added layout to _app

--- pages/_app.js
+++ pages/_app.js
@@ -3,106 +3,84 @@ import Cookies from "universal-cookie"
 import axios from "axios"
 import "../styles/globals.css"
 import { useRouter } from "next/router"
-
-import Navbar from "../components/Misc/Navbar"
-import { Provider } from "react-redux"
-
 import { useEffect, useState } from "react"
-import { getAllAPIsApi, useDashboardApi } from "../utils/ApiCalls"
-import {
-	checkStatus,
-	displayErrorToast,
-	getGeneralApiParams,
-	logOutRemoveCookies,
-} from "../utils/GeneralVariables"
-import SideBar from "../components/SideBar"
+import { Provider } from "react-redux"
 import { store } from "../Redux/store"
 import { ConfigProvider } from "react-avatar"
-import Head from "next/head"
 
-function MyApp({ Component, pageProps }) {
+import Navbar from "../components/Misc/Navbar"
+import { getAllAPIsApi } from "../utils/ApiCalls"
+import {
+	checkStatus,
+	getGeneralApiParams,
+} from "../utils/GeneralVariables"
+import SideBar from "../components/SideBar"
+
+const Layout = ({ children }) => {
 	const router = useRouter()
 	const cookies = new Cookies()
-	// to get header from cookies by default
-	axios.defaults.headers.common["Authorization"] =
-		"Bearer " + cookies.get("cookies-token")
 
-	// trigger to show api list
 	const [apiList, setApiList] = useState(false)
-	// allApis are the fetched apis
 	const [allApis, setAllApis] = useState([])
 	const [loading, setLoading] = useState(true)
+
 	let token = cookies.get("cookies-token")
 	let name = cookies.get("cookies-name")
 
-	// const { permissions, isLoading, errorMessage } = useDashboardApi()
+	useEffect(() => {
+		router.pathname !== "/login" && getSidebarItems()
+	}, [token, router])
 
 	const getSidebarItems = async () => {
 		const { baseUrl, headers } = getGeneralApiParams()
 		await getAllAPIsApi(baseUrl, headers).then((response) => {
 			let status = checkStatus(response, "")
 			setLoading(false)
-			// status ? getCurrentAddress(response.data.data.apis, status) : "";
 		})
 	}
 
-	// const getCurrentAddress = (array, status) => {
-	// 	let path = getPathVariable();
-	// 	if (array.some((e) => e.endpoint === path || path === "/admin")) {
-	// 		/* response contains the element we're looking for */
-	// 		setLoading(false);
-	// 		status && setAllApis(array);
-	// 	} else {
-	// 		displayErrorToast(
-	// 			"Unauthorized to use this feature, You have been logged out"
-	// 		);
-	// 		logOutRemoveCookies();
-	// 		router.push("/login");
-	// 	}
-	// };
+	return (
+		<div className="min-h-screen w-screen flex">
+			{router.pathname !== "/login" && (
+				<div className="pb-12 z-50">
+					<Navbar name={name} />
+				</div>
+			)}
+			{router.pathname !== "/login" && (
+				<section className="z-40">
+					<SideBar
+						apiList={apiList}
+						setApiList={setApiList}
+						allApis={allApis}
+						setAllApis={setAllApis}
+					/>
+				</section>
+			)}
+			<section
+				className={
+					router.pathname === "/login"
+						? "grow"
+						: "grow ml-16 mt-12"
+				}
+			>
+				{children}
+			</section>
+			<ToastContainer />
+		</div>
+	)
+}
 
-	// const getPathVariable = () => {
-	// 	let path = window.location.pathname;
-	// 	if (path === "/admin") return "/admin";
-	// 	let newPath = path.slice(6);
-	// 	return newPath;
-	// };
-
-	useEffect(() => {
-		router.pathname !== "/login" && getSidebarItems()
-	}, [token, router])
+function MyApp({ Component, pageProps }) {
+	const cookies = new Cookies()
+	axios.defaults.headers.common["Authorization"] = "Bearer " + cookies.get("cookies-token")
 
 	return (
 		<Provider store={store}>
 			<ConfigProvider colors={["blue", "mediumblue", "darkblue", "navy"]}>
-				<div className="min-h-screen w-screen flex">
-					{router.pathname !== "/login" && (
-						<div className="pb-12 z-50">
-							<Navbar name={name} />
-						</div>
-					)}
-					{router.pathname !== "/login" && (
-						// <SideBar apiList={apiList} setApiList={setApiList} allApis={allApis} />
-						<section className="z-40">
-							<SideBar
-								apiList={apiList}
-								setApiList={setApiList}
-								allApis={allApis}
-								setAllApis={setAllApis}
-							/>
-						</section>
-					)}
-					<section
-						className={
-							router.pathname === "/login"
-								? "grow"
-								: "grow ml-16 mt-12"
-						}
-					>
-						<Component {...pageProps} />
-					</section>
+				<Layout>
+					<Component {...pageProps} />
 					<ToastContainer />
-				</div>
+				</Layout>
 			</ConfigProvider>
 		</Provider>
 	)
